86c8e879e943c6e91a49d520628d3931ad9e812e
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-01-22 15:11:44 +0545

    Connect to mongodb

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,3 +1,12 @@
 import mongoose from "mongoose";
 
 // Connect to mongodb
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./config/db.config.js";
 
+dotenv.config();
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,5 +18,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server started on port ${PORT}`);
 });
