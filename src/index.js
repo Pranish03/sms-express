9d463613602157d09422a98f85b6f41b7e7018f8
bipin-pokharel <pokharelbipin850@gmail.com>
2026-02-07 14:49:52 +0545

    Update cors in index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
 import { connectDB } from "./config/db.config.js";
-import authRouter from "./routes/auth.routes.js";
 import adminRouter from "./routes/admin.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
